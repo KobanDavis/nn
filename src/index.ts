3b81c45fda04a91cec3e2e9f4d36a3cc0702bfca
Koban Davis <koban.davis@mondago.com>
2020-08-11 12:26:02 +0100

    use correct import in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as NeuralNetwork } from './nn'
 export { default as ActivationFunction } from './activationFunction'
 export { default as Layer } from './layer'
-export { default as Matrix } from './Matrix'
+export { default as Matrix } from './matrix'
